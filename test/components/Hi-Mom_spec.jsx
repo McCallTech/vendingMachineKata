aab4c59e49cee03d730233d87f750c2d42cd89a7
Josh McCall <joshmccall221@gmail.com>
2016-06-08 23:59:24 -0500

    Passing Test: refactored keeping green test green

--- test/components/Hi-Mom_spec.jsx
+++ test/components/Hi-Mom_spec.jsx
@@ -6,14 +6,21 @@ import {
 } from 'react-addons-test-utils';
 import HiMom from '../../src/components/Hi-Mom';
 import {expect} from 'chai';
+
+let expectedProps;
+
 describe('When Hi-Mom component is rendered:', () => {
     it('should render "Hi-Mom" component with h1 heading w/ "Hi Mom!"', () => {
 
+        expectedProps = {
+            className: 'hi-mom',
+            text : 'Hi Mom!'
+        };
 
         const component = renderIntoDocument(
             <HiMom 
-                className='hi-mom'
-                text='Hi Mom!'
+                className={expectedProps.className}
+                text={expectedProps.text}
             />
         );
         const hiMomHeader = scryRenderedDOMComponentsWithTag(component, 'h1');
@@ -21,7 +28,7 @@ describe('When Hi-Mom component is rendered:', () => {
         let  [h1] = hiMomHeader; 
 
         expect(hiMomHeader.length).to.eql(1);
-        expect(h1.className).to.eql('hi-mom');
-        expect(h1.textContent).to.eql('Hi Mom!');
+        expect(h1.className).to.eql(expectedProps.className);
+        expect(h1.textContent).to.eql(expectedProps.text);
     });
 });
