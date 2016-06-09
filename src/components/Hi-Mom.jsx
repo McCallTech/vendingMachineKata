f9ab77d2e64ead6e8cc24997638385445debd14c
Josh McCall <joshmccall221@gmail.com>
2016-06-09 00:05:06 -0500

    Passing Test: refactored Hi-Mom using destructuring for readability

--- src/components/Hi-Mom.jsx
+++ src/components/Hi-Mom.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default React.createClass({
   render: function() {
+    let {className, text} = this.props;
     return <div className="hi-mom">
-        <h1 className= {this.props.className}>
-            {this.props.text} 
+        <h1 className= {className}>
+            {text} 
         </h1>
     </div>;
   }
