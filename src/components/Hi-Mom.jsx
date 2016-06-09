437b4a1a91953f0cbe7ae8eb339561a5017b698a
Josh McCall <joshmccall221@gmail.com>
2016-06-09 00:06:06 -0500

    Passing Test: removed unnecessary markup

--- src/components/Hi-Mom.jsx
+++ src/components/Hi-Mom.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 export default React.createClass({
   render: function() {
     let {className, text} = this.props;
-    return <div className="hi-mom">
-        <h1 className= {className}>
+    return <h1 className= {className}>
             {text} 
-        </h1>
-    </div>;
+    </h1>;
   }
 });
