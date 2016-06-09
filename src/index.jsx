7a1c5fcee84432a443754149483d17017c197b74
Josh McCall <joshmccall221@gmail.com>
2016-06-08 22:00:09 -0500

    webpack hot loading, react, hello world

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HiMom from './components/Hi-Mom';
+
+
+ReactDOM.render(
+  <HiMom/>,
+  document.getElementById('app')
+);
