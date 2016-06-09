a36604ec7248ee43fdd3a5f4f8079e520426aaed
Josh McCall <joshmccall221@gmail.com>
2016-06-08 22:40:26 -0500

    Failing Test: Should render only 1 h1 tag

--- src/components/Hi-Mom.jsx
+++ src/components/Hi-Mom.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 export default React.createClass({
   render: function() {
     return <div className="hi-mom">
-        <h1>
-           Hi, Mom!
-        </h1>
     </div>;
   }
 });
--- test/components/Hi-Mom_spec.jsx
+++ test/components/Hi-Mom_spec.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { renderIntoDocument } from 'react-addons-test-utils';
+import { 
+    renderIntoDocument,
+    scryRenderedDOMComponentsWithTag
+} from 'react-addons-test-utils';
 import HiMom from '../../src/components/Hi-Mom';
 import {expect} from 'chai';
 describe('Hi-Mom', () => {
     it('should render "Hi-Mom"', () => {
-        expect(1).to.eql(1);
+        const component = renderIntoDocument(<HiMom/>);
+        const hiMomHeader = scryRenderedDOMComponentsWithTag(component, 'h1');
+        //console.log(hiMomHeader); 
+        expect(hiMomHeader.length).to.eql(1);
 
     });
 });
