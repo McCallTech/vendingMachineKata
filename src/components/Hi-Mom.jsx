eb5304dd891be2c88cca643a8c9cc3888abb96cf
Josh McCall <joshmccall221@gmail.com>
2016-06-08 23:46:46 -0500

    Passing Test: expect h1 to have a class of hi-mom

--- src/components/Hi-Mom.jsx
+++ src/components/Hi-Mom.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default React.createClass({
   render: function() {
     return <div className="hi-mom">
-        <h1>
+        <h1 className= {this.props.className}>
             {this.props.text} 
         </h1>
     </div>;
