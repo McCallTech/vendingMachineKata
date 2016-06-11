8fe993040ba5369d6cb689df074ae2deffcdf93f
joshmccall221 <joshmccall221@gmail.com>
2016-06-11 12:06:40 -0500

    Update deploy-ghpages.sh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-rm -rf out || exit 0;
-mkdir out; 
-node build.js
-( cd out
- git init
- git config user.name "Travis-CI"
- git config user.email "travis@mccalltech.com"
- cp ../index.html ./index.html
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+#rm -rf out || exit 0;
+#mkdir out; 
+#node build.js
+cd dist
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@mccalltech.com"
+#cp ../index.html ./index.html
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
