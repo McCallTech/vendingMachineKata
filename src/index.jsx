5afc9ceafc04d96fe3006a032fb0fee0a36598a4
joshmccall221 <joshmccall221@gmail.com>
2016-06-12 03:44:44 -0500

    testing docker things

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import HiMom from './components/Hi-Mom';
 
 let expectedProps = {
     className: 'hi-mom',
-    text : 'Momma, look what I can do!!!!!'
+    text : 'Momma, look over here ================>>>>>>>>>>>>!!!!!'
 };
 
 
