77dc5f724b8067068b7575ddfc2c4a78fde82ef3
joshmccall221 <joshmccall221@gmail.com>
2016-06-13 17:13:24 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import HiMom from './components/Hi-Mom';
 
 let expectedProps = {
     className: 'hi-mom',
-    text : 'Momma, look over here ================>>>>>>>>>>>>!!!!!'
+    text : 'Hello World!!'
 };
 
 
